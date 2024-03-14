eb65f38b267c8e5b5e2de96df17d3947296a6452
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2024-03-14 15:38:09 -0400

    set up p5

--- cypress.config.js
+++ cypress.config.js
@@ -8,10 +8,8 @@ module.exports = defineConfig({
       require('cypress-fail-fast/plugin')(on, config)
     },
     env: {
-      // we want to run other specs even if some tests fail
-      // so our strategy is to skip the rest of the tests
-      // in the current spec
-      FAIL_FAST_STRATEGY: 'spec',
+      // if even a single test fails, we want to skip
+      // the rest of all tests and specs on this machine
     },
   },
 })
